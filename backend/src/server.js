a7852b1cb9ac6149a9ee32a5933bd4da81f63e69
AdeibArief <sadeib3@gmail.com>
2026-01-13 07:36:18 +0530

    Allow all CORS origins

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,22 +18,7 @@ const allowedOrigins = [
   "https://mern-ecommerce-sales-horse.vercel.app", // Production URL (if different)
 ];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // Allow requests with no origin (like mobile apps or curl)
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        console.log("Blocked origin:", origin);
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
